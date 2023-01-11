9bf5a5a8223f0aaa53894da0ff9908a36d106f0f
terry <guochi.qin@outlook.com>
2023-01-11 15:21:49 +0800

    update

--- src/components/07-事件处理.vue
+++ src/components/07-事件处理.vue
@@ -32,6 +32,68 @@
             keyEnter() {
                 console.log("回车按键");
             }
+        },
+
+        // 一个组件需要显式声明它所接受的 props，这样 Vue 才能知道外部传入的哪些是 props，哪些是透传 attribute
+        // props 会暴露到 this 上（可以理解为相当于在 data 选项中定义了这个属性，用法跟 data 选项中的属性一样）
+        
+        // 声明 props 的方式一：使用字符串数组
+        // props: ["propA", "propB"]
+
+        // 声明 props 的方式二：使用对象
+        // 对于以对象形式声明中的每个属性，key 是 prop 的名称，而值则是该 prop 预期类型的构造函数
+        // 使用对象形式时，可以进行额外的操作，例如：指定属性的类型、默认值、是否必传等
+
+        /**
+         * 单向数据流：
+         *      所有的 props 都遵循着单向绑定原则，props 因父组件的更新而变化，自然地将新的状态向下流往子组件，而不会逆向传递。
+         *      这避免了子组件意外修改父组件的状态的情况，不然应用的数据流将很容易变得混乱而难以理解。
+         */
+
+        props: {
+            // 单一类型检查（如果传过来的值为 `null` 或 `undefined` 时，则会跳过任何类型检查）
+            propA: String,
+            // 多种类型检查
+            propB: [String, Number],
+            // 类型检查 & 默认值
+            propC: {
+                type: Number,
+                // 当父组件没有传值过来时，使用 default 指定的值
+                default: 100
+            },
+            // 属性是否必传
+            propD: {
+                type: String,
+                // 必传，且类型必须是 String
+                required: true
+            },
+            // 属性的类型为对象
+            propE: {
+                type: Object,
+                // 对象或者数组应当用工厂函数返回
+                default() {
+                    return {};
+                }
+            },
+            // 属性的类型为数组
+            propF: {
+                type: Array,
+                // 对象或者数组应当用工厂函数返回
+                default() {
+                    return [];
+                }
+            }
+        },
+
+        mounted() {
+            console.log("子组件访问父组件");
+            console.log(this.$parent);
+
+            console.log("子组件访问父组件中的对象");
+            console.log(this.$parent.propE);
+
+            console.log("子组件访问根组件");
+            console.log(this.$root);
         }
     }
 </script>
@@ -90,6 +152,17 @@
                 .right
         -->
         <p>监听回车按键：<input type="text" name="msg" v-on:keyup.enter="keyEnter"></p>
+    </div>
+
+    <!-- 传递 props -->
+    <div>
+        <h3>传递 props</h3>
+        <p>父组件传递过来的数据（propA）：{{propA}}</p>
+        <p>父组件传递过来的数据（propB）：{{propB}}</p>
+        <p>父组件传递过来的数据（propC）：{{propC}}</p>
+        <p>父组件传递过来的数据（propD）：{{propD}}</p>
+        <p>父组件传递过来的数据（propE）：{{propE}}</p>
+        <p>父组件传递过来的数据（propF）：{{propF}}</p>
     </div>
 </template>
 
--- src/components/09-组件基础.vue
+++ src/components/09-组件基础.vue
@@ -1,18 +1,104 @@
 <script>
+
+    /**
+     * 组件：带有名称的可复用实例，是某一单一功能的封装；
+     * 
+     * 组件就类似于 Java 中一个 JavaBean 下，可以存在多个别的 JavaBean 的引用，例如这种结构：
+     * 
+     *      public class 保单类 {
+     *          private 投保人类 a;
+     *          private 被保人类 b;
+     *          private 保单明细 c;
+     *      }
+     * 
+     * 即：一个大功能，由多个小功能组成；
+     */
+
+    // 组件引入语法：import 组件别名 from '组件路径'
+    import demo05 from './05-条件渲染.vue'
+    import demo07 from './07-事件处理.vue'
+
     export default{
+        /**
+         * 为什么 default 对象中的 data 必须是一个函数，并且要返回一个对象的原因：为了组件进行多次复用时，每个组件都能拿到一个独立的 data 数据，防止数组污染（类似于：局部变量和全局变量）
+         */
         data() {
             return {
                 name: "tom",
-                age: 20
+                age: 20,
+                // data 中声明的属性，可以传递到子组件中
+                propA: "propA",
+                propC: 10,
+                propD: "propD",
+                propE: {
+                    name: "zhangsan",
+                    age: 18
+                },
+                propF: ["北京","上海","广州"]
             }
+        },
+
+        components: {
+            // 组件引入后还不能直接使用，需要将组件暴露给模板（我们需要在 components 选项上注册它。这个组件将会以其注册时的名字作为模板中的标签名）
+            demo05,
+            demo07
+        },
+
+        mounted() {
+            console.log("父组件访问子组件");
+            console.log(this.$refs);
         }
     }
 </script>
 
 <template>
+    <!-- 组件复用 -->
     <div>
-        <p>{{ name }}</p>
-        <p>{{ age }}</p>
+        <!-- 要复用对应的组件时，直接通过 <组件别名></组件别名> 的方式使用-->
+        <hr>
+        <h3>展示 demo05 组件</h3>
+        <demo05 ref="demo05"></demo05>
+    </div>
+
+    <!-- 在当前的 "09-组件基础" 中，"demo05" 为 "09-组件基础" 的子组件  -->
+    <!-- 传递 props：将父组件中的数组传递到自组件中 -->
+    <div>
+        <!-- 当一个值被传递给 prop 时，它将成为该组件实例上的一个属性。该属性的值可以像其他组件属性一样，在模板和组件的 this 上下文中访问 -->
+        <!-- 可以使用动态绑定或者直接传值 -->
+        <hr>
+        <h3>展示 demo07 组件</h3>
+        <demo07 ref="demo07" v-bind:propA="propA" propB="hello props" v-bind:propD="propD" v-bind:propE="propE" v-bind:propF="propF"></demo07>
+    </div>
+
+    <!-- 父子组件相互访问 -->
+    <div>
+        <!-- 
+            父组件访问自组件：$refs 
+                1）在子组件标签上添加 ref 属性（类似于 id 的作用）
+                2）父组件中使用 this.$refs 即可访问到对应的子组件
+                3）案例：
+                    <demo05 ref="demo05"></demo05>
+                    console.log("父组件访问子组件");
+                    console.log(this.$refs);
+        -->
+
+        <!-- 
+            子组件访问父组件：$parent 
+                1）案例：
+                    console.log("子组件访问父组件");
+                    console.log(this.$parent);
+
+                    console.log("子组件访问父组件中的对象");
+                    console.log(this.$parent.propE);
+
+        -->
+
+        <!-- 
+            子组件访问跟组件：$root 
+                1）案例：
+                    console.log("子组件访问根组件");
+                    console.log(this.$root);
+        -->
     </div>
 </template>
 
